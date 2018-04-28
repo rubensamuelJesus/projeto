6670f7335a559a626cc7b915262e600bda913762
rukuyra <rubensamuel_jesus@hotmail.com>
2018-04-28 17:22:18 +0100

    corrigido html/css(teste) - ainda falta mais

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -41,11 +41,6 @@
 
 <div class="wrapper">
     <div class="sidebar" data-background-color="white" data-active-color="danger">
-
-    <!--
-		Tip 1: you can change the color of the sidebar's background using: data-background-color="white | black"
-		Tip 2: you can change the color of the active button using the data-active-color="primary | info | success | warning | danger"
-	-->
     	<div class="sidebar-wrapper">
             <div class="logo">
                 <a href="/" class="simple-text">
