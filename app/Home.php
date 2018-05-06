e035c8cf573916341583cd1c37fa7cf0336ebd88
rukuyra <rubensamuel_jesus@hotmail.com>
2018-05-06 20:34:56 +0100

    Home model

---
+++ app/Home.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Home extends Model
+{
+   	// Overrides table name
+	protected $table = 'users';
+	// Overrides primary key
+	protected $primaryKey = 'product_code';
+	// Disables auto timestamps
+	public $timestamps = false;
+}
