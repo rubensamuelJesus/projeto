4ed8b6a769470bcd4057258fe8336fa904fa4ec9
xa xa <xa-fc@live.com.pt>
2018-06-09 20:07:13 +0100

    search feito pra user normal, pro admin ta igual mas n ta feito como ta no US 6 ,nem ta com a rota /users

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -31,84 +31,131 @@ class ProfilesController extends Controller
         }
     }
 
-    public function index()
-    {
-        $users_all = Profiles::all();
-        $user = Auth::user();
-        //variavel de teste ----- $user_teste = User::find(28);
-        $ids_associated_members = null;
-        $ids_associated_belong = null;
-        $associated_members = null;
-        $associated_members_belong = null;
+    public function query_normal(Request $request){
 
-        if($user->associate_members->count()){
-            foreach ($user->associate_members as $associate)
-            {
-                $ids_associated_members[] = $associate->associated_user_id;
-            } 
-            $associated_members = User::select()
-                    ->whereIn('id', $ids_associated_members)
-                    ->get(); 
-        }
-
-        $user_associated_member = Associate_members::select()
-                    ->where('associated_user_id',$user->id)
-                    ->get(); 
-
-        function deleteElement($element, &$array){
-            $index = array_search($element, $array);
-            if($index !== false){
-                unset($array[$index]);
-            }
-        }
-                    
-        if($user_associated_member->count()){
-            foreach ($user_associated_member as $associate)
-            {
-                $ids_associated_members_belong[] = $associate->main_user_id;
-            } 
-
-            $associated_members_belong = User::select()
-                    ->whereIn('id', $ids_associated_members_belong)
+        if ($request->filled('name')) {
+            $users_all = User::select()
+                    ->where('name', 'like', '%'.$request->name.'%')
                     ->get();
 
-            foreach ($associated_members_belong as $associated_member_belong)
+            $user = Auth::user();
+            //variavel de teste ----- $user_teste = User::find(28);
+            $ids_associated_members = null;
+            $ids_associated_belong = null;
+            $associated_members = null;
+            $associated_members_belong = null;
 
-                foreach ($associated_member_belong->associate_members as $associated_member_belong1) {
-                    {
-                    $ids_associated_members_belong[] = $associated_member_belong1->associated_user_id;
+            if($user->associate_members->count()){
+                foreach ($user->associate_members as $associate)
+                {
+                    foreach ($users_all as $user_query) {
+                        if ($associate->associated_user_id == $user_query->id) {
+                            $ids_associated_members[] = $associate->associated_user_id;
+                        }
+                    }
+                } 
+                $associated_members = User::select()
+                        ->whereIn('id', $ids_associated_members)
+                        ->get(); 
+            }
+
+            $user_associated_member = Associate_members::select()
+                        ->where('associated_user_id',$user->id)
+                        ->get(); 
+
+            function deleteElement($element, &$array){
+                $index = array_search($element, $array);
+                if($index !== false){
+                    unset($array[$index]);
                 }
             }
-            $ids_associated_members_belong = array_unique($ids_associated_members_belong);
-            deleteElement($user->id, $ids_associated_members_belong);
+                        
+            if($user_associated_member->count()){
+                foreach ($user_associated_member as $associate)
+                {
+                    foreach ($users_all as $user_query) {
+                        if ($associate->main_user_id == $user_query->id) {
+                            $ids_associated_members_belong[] = $associate->main_user_id;
+                        }
+                    }
+                } 
 
-            $associated_members_belong = User::select()
-                    ->whereIn('id', $ids_associated_members_belong)
-                    ->get();
+                $associated_members_belong = User::select()
+                        ->whereIn('id', $ids_associated_members_belong)
+                        ->where('name', 'like', '%'.$request->name.'%')
+                        ->get();
+
+                foreach ($associated_members_belong as $associated_member_belong){
+                    foreach ($associated_member_belong->associate_members as $associated_member_belong1) {
+                        foreach ($users_all as $user_query) {
+                            if ($associated_member_belong1->id == $user_query->id) {
+                                $ids_associated_members_belong[] = $associated_member_belong1->associated_user_id;
+                            }
+                        }
+                    }
+                }
+                $ids_associated_members_belong = array_unique($ids_associated_members_belong);
+                deleteElement($user->id, $ids_associated_members_belong);
+
+                $associated_members_belong = User::select()
+                        ->whereIn('id', $ids_associated_members_belong)
+                        ->get();
+            }
+        }else{
+            $users_all = Profiles::all();
+            $user = Auth::user();
+            //variavel de teste ----- $user_teste = User::find(28);
+            $ids_associated_members = null;
+            $ids_associated_belong = null;
+            $associated_members = null;
+            $associated_members_belong = null;
+
+            if($user->associate_members->count()){
+                foreach ($user->associate_members as $associate)
+                {
+                    $ids_associated_members[] = $associate->associated_user_id;
+                } 
+                $associated_members = User::select()
+                        ->whereIn('id', $ids_associated_members)
+                        ->get(); 
+            }
+
+            $user_associated_member = Associate_members::select()
+                        ->where('associated_user_id',$user->id)
+                        ->get(); 
+
+            function deleteElement($element, &$array){
+                $index = array_search($element, $array);
+                if($index !== false){
+                    unset($array[$index]);
+                }
+            }
+                        
+            if($user_associated_member->count()){
+                foreach ($user_associated_member as $associate)
+                {
+                    $ids_associated_members_belong[] = $associate->main_user_id;
+                } 
+
+                $associated_members_belong = User::select()
+                        ->whereIn('id', $ids_associated_members_belong)
+                        ->get();
+
+                foreach ($associated_members_belong as $associated_member_belong)
+
+                    foreach ($associated_member_belong->associate_members as $associated_member_belong1) {
+                        {
+                        $ids_associated_members_belong[] = $associated_member_belong1->associated_user_id;
+                    }
+                }
+                $ids_associated_members_belong = array_unique($ids_associated_members_belong);
+                deleteElement($user->id, $ids_associated_members_belong);
+
+                $associated_members_belong = User::select()
+                        ->whereIn('id', $ids_associated_members_belong)
+                        ->get();
+            }
         }
-        //return $associated_members_belong;
-        //return "vazio";
-
-        //return $user_associated_member->associated_user_id;
-
         return view('profiles', compact('users_all','user','associated_members','associated_members_belong'));
-       
-    }
-
-    public function query_normal(Request $request)
-    {
-        $query_users = User::select()
-                ->where('name', 'like', '%'.$request->name.'%')
-                ->get(); 
-        return view('profiles', compact('query_users'));
-    
-    }
-    
-    public function query_admin()
-    {
-        
-
-        return view('profiles', compact('users_all','user','associated_members','associated_members_belong'));
-       
     }
 }
